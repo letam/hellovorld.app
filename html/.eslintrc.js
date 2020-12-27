89242f5d9460647c57448ac0b37a7c2b3059427c
Tam Le <tamsgit@gmail.com>
2020-12-27 02:52:50 +0000

    Initialize web app project with node.js and yarn

---
+++ html/.eslintrc.js
@@ -0,0 +1,17 @@
+/**
+ * Configuration settings for eslint
+ */
+
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {},
+}
